c8b7f58510cbb5d5e60095c664ee122193660631
Sayantan Mridha <smridha@asu.edu>
2021-04-06 23:27:59 -0700

    Update settings.py

--- sraToolDjango/settings.py
+++ sraToolDjango/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/all")
 
 MEDIA_URL = "/images/"
 
