d00a1fc68f896189519c4a7c1d54c6827cd26b87
Sayantan Mridha <smridha@asu.edu>
2021-04-27 02:31:20 -0700

    Added Links and Detailed Views to Elements (#4)
    * Update .gitignore (#1)
    
    * Updated requirements.txt
    
    * Added Links and Detailed Views to Elements
    
    Details Added to
    - Products
    - Departments
    - Vendors

--- vendor/models.py
+++ vendor/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
+from django.db.models.fields import SlugField
 from accounts.models import Account
 
 # Create your models here.  
@@ -15,6 +16,7 @@ class Vendor(models.Model):
     name = models.CharField("Vendor Name", max_length=150, null=True)
     website = models.CharField("Vendor Website", max_length=200, null=True)
     exstContract = models.CharField("Do you have an existing signed contract with the Company?", max_length=25, null=True, choices=CATEGORY)
+    slug = models.SlugField(verbose_name="Vendor Slug", max_length=175, blank=True)
 
     def __str__(self) -> str:
         return self.name
